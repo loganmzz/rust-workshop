9a5f0f1f87d92b13dc65ffad55e327554e706d5d
Logan Mzz <loganmzz@users.noreply.github.com>
2017-10-23 12:47:13 +0200

    Adds '01 basic' exercices

---
+++ 01-basic/src/lib.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code)]
+
+fn get_false() {
+}
+
+fn get_42i32() {
+}
+
+fn get_pi() {
+}
+
+fn get_unit() -> usize {
+    8
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_false_should_return_false_as_bool() {
+        let result: bool = get_false();
+        assert_eq!(false, result);
+    }
+
+    #[test]
+    fn get_42i32_should_return_42_as_i32() {
+        let result: i32 = get_42i32();
+        assert_eq!(42, result);
+    }
+
+    #[test]
+    fn get_pi_should_return_314_as_f64() {
+        let result: f64 = get_pi();
+        assert_eq!(3.14, result);
+    }
+
+    #[test]
+    fn get_unit_should_return_unit() {
+        let result: () = get_unit();
+        assert_eq!((), result);
+    }
+
+    #[test]
+    fn add_with_8u8_1024u16_should_return_1032u32() {
+        let result: u32 = add(8u8, 1024u16);
+        assert_eq!(1032, result);
+    }
+}
