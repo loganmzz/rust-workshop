6f09dbc63f8427763fd0a017cb932956f35a48e8
Alessio Coltellacci <lightplay8@gmail.com>
2018-04-15 15:17:22 +0200

    Add the last challenge: make a PR on the project

---
+++ 14-challenge/src/lib.rs
@@ -0,0 +1,24 @@
+//! 14 Challenge
+//! --------------
+//!
+//! Welcome to fourteenth step of this Rust workshop.
+//!
+//! The task is to make a **Pull Request** on the rust project `please`, the universal project builder.
+//!
+//! # Purpose:
+//!
+//! With `please`, switching from one technology to the next is easy, no need to remember all the commands!
+//! Write `please build` to build the project, `please run` to run it! Works with every language available.
+//!
+//! Right now, only `rust` is supported. So the task is to support a new language.
+//! You pick the one your prefer and make a `PR` for him.
+//!
+//! **Clone the repo**: `git clone https://github.com/RustBeginners/please`
+//!
+//! # Tips
+//!
+//! * Create a new **Platform** in `src/platform/<my_platform>.rs`
+//!
+//! * Implement the `trait Platform` define in [src/platform/mod.rs](https://github.com/RustBeginners/please/blob/master/src/platform/mod.rs)
+//!
+//! * Get an exemple (rust) of a platform implementation here: [src/platform/rust.rs](https://github.com/RustBeginners/please/blob/master/src/platform/rust.rs)
