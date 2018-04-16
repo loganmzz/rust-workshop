8d2884e1dee97451487a8a13b8a9a17b9dd4a750
Logan Mzz <loganmzz@users.noreply.github.com>
2018-04-16 13:13:51 +0200

    #6 Moves challenge to last (99)

--- 99-challenge/src/lib.rs
+++ 99-challenge/src/lib.rs
@@ -1,17 +1,17 @@
-//! 14 Challenge
+//! 99 Challenge
 //! --------------
 //!
-//! Welcome to fourteenth step of this Rust workshop.
+//! Welcome to last step of this Rust workshop.
 //!
 //! The task is to make a **Pull Request** on the rust project `please`, the universal project builder.
 //!
-//! # Purpose:
+//! # Purpose
 //!
 //! With `please`, switching from one technology to the next is easy, no need to remember all the commands!
 //! Write `please build` to build the project, `please run` to run it! Works with every language available.
 //!
 //! Right now, only `rust` is supported. So the task is to support a new language.
-//! You pick the one your prefer and make a `PR` for him.
+//! You pick the one your prefer and make a `PR` for it.
 //!
 //! **Clone the repo**: `git clone https://github.com/RustBeginners/please`
 //!
@@ -21,4 +21,4 @@
 //!
 //! * Implement the `trait Platform` define in [src/platform/mod.rs](https://github.com/RustBeginners/please/blob/master/src/platform/mod.rs)
 //!
-//! * Get an exemple (rust) of a platform implementation here: [src/platform/rust.rs](https://github.com/RustBeginners/please/blob/master/src/platform/rust.rs)
+//! * Get an example (rust) of a platform implementation here: [src/platform/rust.rs](https://github.com/RustBeginners/please/blob/master/src/platform/rust.rs)
